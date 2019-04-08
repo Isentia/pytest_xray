ba4d62c8af81dad88a81a067b4de44f6521d48d1
Neville Tummon <nt.devs@gmail.com>
2019-04-08 20:15:37 +1000

    add bumpversion and versions to project

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="myproject",
+    version="0.1"
     packages=["myproject"],
     install_requires=["pytest"],
     summary="py.test Xray integration plugin, using markers",
