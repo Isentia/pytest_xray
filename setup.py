2a8287fd034c24db717b6aa0928789230e4f4a72
Neville Tummon <nt.devs@gmail.com>
2019-04-08 20:35:14 +1000

    add readme

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+
+def long_description():
+    with open("README.md") as f:
+        return f.read()
+
+
 setup(
     name="myproject",
-    version="0.1"
+    version="0.1",
+    long_description=long_description(),
     packages=["myproject"],
     install_requires=["pytest"],
     summary="py.test Xray integration plugin, using markers",
