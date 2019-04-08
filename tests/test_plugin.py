3f975d29d5e60b535f843f84f88ae75b2385fc1c
Neville Tummon <nt.devs@gmail.com>
2019-04-08 16:18:10 +1000

    move test file

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,35 +1,30 @@
-import logging
-import unittest
-from random import randint
-
-import pytest
-
-pytest_plugins = "pytester"
-# from src import pytest_xray
-
-logging.basicConfig(level=logging.INFO)
+pytest_plugins = "pytester"  # to get testdir fixture
 
 
-class PluginTests(unittest.TestCase):
+def test_myplugin(testdir):
+    test_example_1 = """
+    from random import randint
+
+    import pytest
+
+    pytest_plugins = "pytester"
+
     @pytest.mark.xray(test_key="PRDS-12277", test_exec_key="PRDS-12276")
-    def test_pass_1(self):
+    def test_pass_1():
         pass
-
-    def test_pass_2(self):
-        pass
-
-    @pytest.mark.xray(test_key="PRDS-12279", test_exec_key="PRDS-12276")
-    def test_fail(self):
-        self.fail("Forced failure 1")
-
-    def test_fail_2(self):
-        self.fail("Forced failure 2")
 
     @pytest.mark.xray(test_key="PRDS-12280", test_exec_key="PRDS-12276")
-    def test_intermittent_failure_1(self):
-        if randint(0, 100) < 20:
-            raise Exception("Something went wrong")
+    def test_pass_2():
+        pass
+    """
+    testdir.makepyfile(test_example_1)
+    result = testdir.runpytest("--jira-xray")
+    # result = testdir.runpytest("")
+    assert len(result.errlines) == 1
+    assert result.errlines[0] == ""
+    result.fnmatch_lines(
+        """
+        test_example*
+    """
+    )
 
-    def test_intermittent_failure_2(self):
-        if randint(0, 100) < 20:
-            raise Exception("Something went wrong")
