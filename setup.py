0910fe31918f446681e8267885230cc53f16fb68
Neville Tummon <nt.devs@gmail.com>
2019-04-16 11:47:13 +1000

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def pkg_install_requires():
 
 setup(
     name="pytest_xray",
-    version="0.1.4",
+    version="0.1.5",
     long_description=long_description(),
     packages=find_packages(exclude=("tests",)),
     install_requires=pkg_install_requires(),
